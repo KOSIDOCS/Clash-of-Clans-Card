a404cdfc41ea483581152bb29014fc7f424a601e
kosidocs <kelvinonaru@gmail.com>
2021-04-17 21:18:43 +0400

    Updates

---
+++ precache-manifest.10cad70fb54642aa138c5582eb0cf865.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f3576d25d26002b5c21ca0840dafd54",
+    "url": "/Clash-of-Clans-Card/index.html"
+  },
+  {
+    "revision": "21ef11d73c9f8b1d4dd9",
+    "url": "/Clash-of-Clans-Card/static/css/main.bf9d6faf.chunk.css"
+  },
+  {
+    "revision": "25f53dc18685ad3bd155",
+    "url": "/Clash-of-Clans-Card/static/js/2.e6c0491f.chunk.js"
+  },
+  {
+    "revision": "727196b53be8768c78b2ea60f81f0ea1",
+    "url": "/Clash-of-Clans-Card/static/js/2.e6c0491f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21ef11d73c9f8b1d4dd9",
+    "url": "/Clash-of-Clans-Card/static/js/main.173cdc70.chunk.js"
+  },
+  {
+    "revision": "11298e139e7d443e5706",
+    "url": "/Clash-of-Clans-Card/static/js/runtime-main.3632a07e.js"
+  },
+  {
+    "revision": "0ded2295a49a092d77a69fc3b83183f4",
+    "url": "/Clash-of-Clans-Card/static/media/dwarf-removebg.0ded2295.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Clash-of-Clans-Card/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Clash-of-Clans-Card/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Clash-of-Clans-Card/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Clash-of-Clans-Card/static/media/slick.f97e3bbf.svg"
+  }
+]);
